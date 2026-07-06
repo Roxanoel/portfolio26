4b5fe0a0217dbd59ef69e722c78458a1b2f43515
Roxane Noël <roxane.noel92@gmail.com>
2026-07-06 10:17:12 -0700

    visual updates + adding playwright

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/open-sauce-two/400.css'
-import '@fontsource/open-sauce-two/600.css'
-import '@fontsource/open-sauce-two/700.css'
 import './index.css'
 import App from './App'
 
